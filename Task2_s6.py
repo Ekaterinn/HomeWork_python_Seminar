a00b4f7c0b638e55c47f03ce8bce4de3d24e0d2d
Ekaterina <tkat1998@mail.ru>
2023-05-11 13:21:32 +0600

    save

--- Task2_s6.py
+++ Task2_s6.py
@@ -5,15 +5,23 @@
 # [0, 5, 6, 2, 7, 7, 8, 1, 1, 9] - 277 -> да
 # [4, 4, 3, 6, 7, 0, 8, 5, 1, 2] - 812 -> нет
 
-from random import *
- 
-a_list = [randint(1, 100) for x in range(1, 101)]
- 
-N = len(a_list)
-for i in range(1, N):
-    for j in range(i, 0, -1):
-        if a_list[j] < a_list[j-1]:
-            a_list[j], a_list[j-1] = a_list[j-1], a_list[j]
-        else:
-            break
-print(a_list)
+import random
+def rand(N = 15):
+    return [random.randint(0,9) for _ in range (N)]
+
+def method(Arr: list, N: int):
+    stringArr = ''.join(str(i) for i in Arr)
+    index = stringArr.find(str(N))
+    if index>=0:
+        print(f'Для числа {N} имеется вхождение, индекс: {index}')
+    else:
+        print(f'Для числа {N} вхождений в списке НЕ имеется') 
+
+Arr = rand()
+print (f'Список случайных чисел: {Arr}')
+
+N = int(input('Введите трехзначное число: '))
+if N<100 or N>999:
+    print(f'Число {N} - не трехзначное!')
+else:
+    method(Arr,N)
