ddd828d603943c133cc7f46f780182844dcdcfcb
Ekaterina <tkat1998@mail.ru>
2023-05-29 22:56:16 +0600

    save

--- Sem7.py
+++ Sem7.py
@@ -1,27 +1,24 @@
 import telebot
 import requests
 
-bot = telebot.TeleBot("6207493106:AAHOIXqwXhnG0b7-tEHfWAKx-EM351arHEU")  #, parse_mode=None
+bot = telebot.TeleBot("")  
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
-
-
-# @bot.message_handler(func=lambda m: True)
-# def echo_all(message):
-# 	bot.reply_to(message, message.text)
 	
 @bot.message_handler(content_types=['text'])
 def greetings(message):
-	# print(message)
-	text = message.text
-	# if 'привет' in text:
-	# 	bot.reply_to(message, f'Привет, {message.from_user.first_name}!')
+    text = message.text
     if text == 'погода':
-        req = requests.get('https://wttr.in/?0T')  
-        bot.reply_to(message, req.text)
-
-
+       req = requests.get('https://wttr.in/?0T')  
+       bot.reply_to(message, req.text)
+    elif text == 'кот':
+       req = requests.get('https://cataas.com/cat')  
+       bot.send_photo(message.from_user.id, req.content)
+ 
 
 bot.polling()
+
+# if 'привет' in text:
+# 		bot.reply_to(message, f'Привет, {message.from_user.first_name}!')
